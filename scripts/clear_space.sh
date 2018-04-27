3c44e08c9614a0867a60eb0aaf51cce6da3b1516
liamtoozer <liam.toozer@gmail.com>
2018-04-27 15:57:59 +0100

    Add teardown pipeline

---
+++ scripts/clear_space.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# cf login -a "$CLOUDFOUNDRY_API" -u "$CLOUDFOUNDRY_EMAIL" -p "$CLOUDFOUNDRY_PASSWORD" -o "$CLOUDFOUNDRY_ORG" -s "$CLOUDFOUNDRY_SPACE" --skip-ssl-validation
+
+APPS=$(cf apps | cut -f 1 -d ' ' | tail -n +5)
+for app in $APPS
+do
+	cf delete -fr "$app"
+done
+
+SERVICES=$(cf services | cut -f 1  -d ' ' | tail -n +5)
+for service in $SERVICES
+do
+	cf delete-service -f "$service"
+done
