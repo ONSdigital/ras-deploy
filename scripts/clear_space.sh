4e3a3039d3971370028a964d84d76efb3e8251d4
Gavin Edwards <gavin.edwards@ons.gsi.gov.uk>
2018-05-01 11:02:48 +0100

    Remove exclusion of rabbitmq service

--- scripts/clear_space.sh
+++ scripts/clear_space.sh
@@ -2,7 +2,13 @@
 
 set -ev
 
-cf login -a "$CLOUDFOUNDRY_API" -u "$CLOUDFOUNDRY_EMAIL" -p "$CLOUDFOUNDRY_PASSWORD" -o "$CLOUDFOUNDRY_ORG" -s "$CLOUDFOUNDRY_SPACE" --skip-ssl-validation
+cf login \
+    -a "$CLOUDFOUNDRY_API" \
+    -u "$CLOUDFOUNDRY_EMAIL" \
+    -p "$CLOUDFOUNDRY_PASSWORD" \
+    -o "$CLOUDFOUNDRY_ORG" \
+    -s "$CLOUDFOUNDRY_SPACE" \
+    --skip-ssl-validation
 
 APPS=$(cf apps | cut -f 1 -d ' ' | tail -n +5)
 
@@ -13,8 +19,8 @@ if [ ${#APPS[@]} -ne 0 ]; then
     done
 fi
 
+SERVICES=$(cf services | cut -f 1 -d ' ' | tail -n +5)
 
-SERVICES=$(cf services | awk '{ print $1 }' | tail -n +5 | grep -v collection-instrument-rabbitmq)
 if [ ${#SERVICES[@]} -ne 0 ]; then
     for service in $SERVICES
     do
