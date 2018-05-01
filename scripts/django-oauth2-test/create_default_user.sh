b9cd82e0b0cde4f5bbdf3771d2a6a16cbe47eec3
Gavin Edwards <gavin.edwards@ons.gsi.gov.uk>
2018-05-01 13:35:53 +0100

    Add script to set up default user

---
+++ scripts/django-oauth2-test/create_default_user.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -ev
+
+APP_NAME=django-oauth2-test-teardown-migration
+
+cf login \
+    -a "$CLOUDFOUNDRY_API" \
+    -u "$CLOUDFOUNDRY_EMAIL" \
+    -p "$CLOUDFOUNDRY_PASSWORD" \
+    -o "$CLOUDFOUNDRY_ORG" \
+    -s "$CLOUDFOUNDRY_SPACE" \
+    --skip-ssl-validation
+
+cf apps >/dev/null # Call this to check that the CF login command was successful
+
+OUTPUT=$(cf run-task "$APP_NAME" ./create_default_user.sh)
+TASK_ID=$(echo "$OUTPUT" | tail -n 1 | sed 's/^task id:  *\([0-9]*\)/\1/')
+
+echo "Ran task with ID: $TASK_ID"
+
+RESULT=
+while [[ -z "$RESULT" || $RESULT == "RUNNING" ]]; do
+    RESULT=$(cf tasks "$APP_NAME" | grep "^$TASK_ID " | awk '{ print $3 }')
+done
+
+if [[ "$RESULT" == 'SUCCEEDED' ]]; then
+    exit 0
+else
+    exit 1
+fi
