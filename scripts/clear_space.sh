fcf61521c8ee6338aa8e37e5eecb3ce57df12b3a
liamtoozer <liam.toozer@gmail.com>
2018-05-01 09:12:33 +0100

    Wrap app & service delete with condition

--- scripts/clear_space.sh
+++ scripts/clear_space.sh
@@ -5,14 +5,20 @@ set -ex
 cf login -a "$CLOUDFOUNDRY_API" -u "$CLOUDFOUNDRY_EMAIL" -p "$CLOUDFOUNDRY_PASSWORD" -o "$CLOUDFOUNDRY_ORG" -s "$CLOUDFOUNDRY_SPACE" --skip-ssl-validation
 
 APPS=$(cf apps | cut -f 1 -d ' ' | tail -n +5)
-for app in $APPS
-do
-	cf delete "$app" -f -r
-done
+
+if [ ${#APPS[@]} -ne 0 ]; then
+    for app in $APPS
+    do
+     cf delete "$app" -f -r
+    done
+fi
+
 
 SERVICES=$(cf services | awk '{ print $1 }' | tail -n +5 | grep -v collection-instrument-rabbitmq)
-for service in $SERVICES
-do
- cf delete-service-key "$service" "$service"-key  -f
-	cf delete-service "$service" -f
-done
+if [ ${#SERVICES[@]} -ne 0 ]; then
+    for service in $SERVICES
+    do
+     cf delete-service-key "$service" "$service"-key  -f
+     cf delete-service "$service" -f
+    done
+fi
