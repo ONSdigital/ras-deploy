45633a23966c0bd015a22277f714bf40f9438f45
gavinleeedwards <gavin.edwards@ons.gsi.gov.uk>
2018-05-29 10:40:43 +0100

    Split acceptance tests between rasrm and secure-message (#10)
    * Add configurable make target
    
    * Split ci acceptance tests into 2 jobs
    
    * Change all resource branches to run from master and amend the ci acceptamce test jobs
    
    * Fix type and remove MAKE_TARGET

--- scripts/rasrm-acceptance-tests/run_acceptance_tests.sh
+++ scripts/rasrm-acceptance-tests/run_acceptance_tests.sh
@@ -7,4 +7,4 @@ source cf-database-env-vars/setenv.sh
 cd rasrm-acceptance-tests-source
 
 pipenv install --dev
-make BEHAVE_FORMAT=pretty setup wait_for_services acceptance_tests
+make BEHAVE_FORMAT=pretty "${MAKE_TARGET}"
