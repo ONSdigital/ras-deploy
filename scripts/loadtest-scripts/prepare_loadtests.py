f2376f9db9b6ec665a93568a748a4aa599d63423
Tom Oram <tom.oram@armakuni.com>
2018-07-20 12:02:20 +0100

    Pass correct arguments to upload_collection_instrument

--- scripts/loadtest-scripts/prepare_loadtests.py
+++ scripts/loadtest-scripts/prepare_loadtests.py
@@ -7,10 +7,12 @@ from clients.collectioninstrumentclient import CollectionInstrumentClient
 from clients.sampleclient import SampleClient
 
 party_url = os.getenv('PARTY_URL', 'http://localhost:8081')
-party_create_respondent_endpoint = os.getenv('PARTY_CREATE_RESPONDENT_ENDPOINT', '/party-api/v1/respondents')
+party_create_respondent_endpoint = os.getenv('PARTY_CREATE_RESPONDENT_ENDPOINT',
+                                             '/party-api/v1/respondents')
 iac_url = os.getenv('IAC_URL', 'http://localhost:8121')
 survey_id = os.getenv('SURVEY_ID', '75b19ea0-69a4-4c58-8d7f-4458c8f43f5c')
-survey_classifiers = os.getenv('SURVEY_CLASSIFIERS', '{"form_type":"0102","eq_id":"1"}')
+survey_classifiers = os.getenv('SURVEY_CLASSIFIERS',
+                               '{"form_type":"0102","eq_id":"1"}')
 username = os.getenv('COLLECTION_INSTRUMENT_USERNAME', 'admin')
 password = os.getenv('COLLECTION_INSTRUMENT_PASSWORD', 'secret')
 polling_wait_time = int(os.getenv('POLLING_WAIT_TIME', '2'))
@@ -21,8 +23,8 @@ ce = CollectionExerciseClient(username, password)
 ci = CollectionInstrumentClient(username, password)
 sample = SampleClient(username, password)
 
-# Enrolment
 
+# Enrolment
 
 def create_enrolment_codes(count):
     url = f'{iac_url}/iacs'
@@ -52,7 +54,8 @@ def with_timeout(action):
         time.sleep(polling_wait_time)
 
 
-def create_user(email_address, first_name, last_name, user_password, telephone, enrolment_code):
+def create_user(email_address, first_name, last_name, user_password, telephone,
+                enrolment_code):
     url = f'{party_url}{party_create_respondent_endpoint}'
     payload = {'emailAddress': email_address,
                'firstName': first_name,
@@ -66,7 +69,8 @@ def create_user(email_address, first_name, last_name, user_password, telephone, 
 
     print(response.status_code)
     if response.status_code != requests.codes.created:
-        error_exit(f'Failed to create user with email {email_address}: {response.text}')
+        error_exit(
+            f'Failed to create user with email {email_address}: {response.text}')
 
     return 'magic link thing'
 
@@ -84,7 +88,7 @@ def upload_and_link_collection_instrument(exercise_id):
     instrument_id = ci.get_collection_id_from_classifier(survey_classifiers)
 
     if instrument_id is None:
-        ci.upload_collection_instrument()
+        ci.upload_collection_instrument(survey_id, survey_classifiers)
         instrument_id = ci.get_collection_id_from_classifier(survey_classifiers)
         print(f'Created collection instrument, ID = {instrument_id}')
     else:
