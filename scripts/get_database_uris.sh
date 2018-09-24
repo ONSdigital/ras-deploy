c6d61b24cdbaab7923abd12bc53afb42d1e037dc
Tejas Patel <tejas.patel@ons.gov.uk>
2018-09-24 09:26:12 +0100

    Add new auth service to pipelines
    Added ras-rm-auth-service to the pipline which will be replacing
    django-oauth-test.

--- scripts/get_database_uris.sh
+++ scripts/get_database_uris.sh
@@ -5,6 +5,6 @@ cf login -a "$CLOUDFOUNDRY_API" -u "$CLOUDFOUNDRY_EMAIL" -p "$CLOUDFOUNDRY_PASSW
 
 echo "export DATABASE_URI=$(cf apps | grep rm-case-service-"$CLOUDFOUNDRY_SPACE" | awk '{ print "cf env "$1 }'| bash | grep "postgres://" | awk -F \" '{ print $4 }')"
 echo "export PARTY_DATABASE_URI=$(cf apps | grep ras-party-"$CLOUDFOUNDRY_SPACE" | awk '{ print "cf env "$1 }'| bash | grep "postgres://" | awk -F \" '{ print $4 }')"
-echo "export DJANGO_OAUTH_DATABASE_URI=$(cf apps | grep django-oauth2-test-"$CLOUDFOUNDRY_SPACE" | awk '{ print "cf env "$1 }'| bash | grep "postgres://" | awk -F \" '{ print $4 }')"
+echo "export AUTH_DATABASE_URI=$(cf apps | grep ras-rm-auth-service-"$CLOUDFOUNDRY_SPACE" | awk '{ print "cf env "$1 }'| bash | grep "postgres://" | awk -F \" '{ print $4 }')"
 echo "export SECURE_MESSAGE_DATABASE_URI=$(cf apps | grep ras-secure-message-"$CLOUDFOUNDRY_SPACE" | awk '{ print "cf env "$1 }'| bash | grep "postgres://" | awk -F \" '{ print $4 }')"
 echo "export COLLECTION_INSTRUMENT_DATABASE_URI=$(cf apps | grep ras-collection-instrument-"$CLOUDFOUNDRY_SPACE" | awk '{ print "cf env "$1 }'| bash | grep "postgres://" | awk -F \" '{ print $4 }')"
